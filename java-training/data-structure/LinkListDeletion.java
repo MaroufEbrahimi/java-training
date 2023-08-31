b7d89a2586307ebdd7fdd8fd6d084bab43002819
Marouf <maroufibrahimi7264@gmail.com>
2023-08-30 20:21:08 -0700

    deleting last Node of the list

--- java-training/data-structure/LinkListDeletion.java
+++ java-training/data-structure/LinkListDeletion.java
@@ -1,18 +1,38 @@
 public class LinkListDeletion {
 
 	private Node first;
-	
+
+	// Deleting first Node of the list
 	public Node deleteFirst() {
-		if(first == null)
+		if (first == null)
 			return null;
-		
+
 		Node temp = first;
 		first = first.next;
 		return temp;
 	}
 
-	public static void main(String[] args) {
+	// Deleting last Node of the list
+	public Node deleteLast() {
+		if (first == null || first.next == null) {
+			return first;
+		}
 
+		Node current = first;
+		Node prev = null;
+		while (current.next != null) {
+			prev = current;
+			current = current.next;
+		}
+
+		prev.next = null;
+		return current;
+	}
+
+	public static void main(String[] args) {
+		LinkListDeletion deletion = new LinkListDeletion();
+		deletion.deleteFirst();
+		deletion.deleteLast();
 	}
 
 }
