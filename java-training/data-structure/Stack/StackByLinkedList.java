95ec49b135c6db54df8715f6ea65391001063d0d
Marouf <maroufibrahimi7264@gmail.com>
2023-09-05 18:27:39 -0700

    added operation of Stack like... isEmpty(), push(), pop(), peek() ect...

--- java-training/data-structure/Stack/StackByLinkedList.java
+++ java-training/data-structure/Stack/StackByLinkedList.java
@@ -1,3 +1,6 @@
+
+import java.util.EmptyStackException;
+
 public class StackByLinkedList {
 
 	private StackNode top;
@@ -11,4 +14,55 @@ public class StackByLinkedList {
 			this.data = data;
 		}
 	}
+
+	public StackByLinkedList() {
+		top = null;
+		size = 0;
+	}
+
+	public int length() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+
+	public void push(int data) {
+		StackNode temp = new StackNode(data);
+		temp.next = top;
+		top = temp;
+		size++;
+	}
+
+	public int pop() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		int res = top.data;
+		top = top.next;
+		size--;
+		return res;
+	}
+
+	public int peek() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return top.data;
+	}
+
+	public static void main(String[] args) {
+		StackByLinkedList stack = new StackByLinkedList();
+		stack.push(2);
+		stack.push(4);
+		stack.push(6);
+
+		System.out.println("The top Element is: " + stack.peek());
+		stack.pop();
+		System.out.println("The top Element is: " + stack.peek());
+		stack.push(8);
+		System.out.println("The top Element is: " + stack.peek());
+	}
+
 }
