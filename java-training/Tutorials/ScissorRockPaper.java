aa25f177610fa807285b301568fcf894e7569edd
Marouf <maroufibrahimi7264@gmail.com>
2023-09-07 22:28:51 -0700

    added condition for scissor

--- java-training/Tutorials/ScissorRockPaper.java
+++ java-training/Tutorials/ScissorRockPaper.java
@@ -10,5 +10,18 @@ public class ScissorRockPaper {
 		var input = new Scanner(System.in);
 		System.out.println("Enter scissor(0), rock(1), paper(2): ");
 		int num = input.nextInt();
+
+		if (randomNum == 0) {
+			if (num == randomNum) {
+				System.out.println("Computer is scissor, you are scissor too! it is a draw");
+			} else {
+				if (num == 1) {
+					System.out.println("Computer is scissor, you are ROCK. you won");
+				}
+				if (num == 2) {
+					System.out.println("Computer is scissor, you are PAPER. Computer won");
+				}
+			}
+		}
 	}
 }
