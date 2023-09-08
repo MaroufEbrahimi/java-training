6d86e63ab0986d8db2f845b2b74c9c4a5f0768ff
Marouf <maroufibrahimi7264@gmail.com>
2023-09-07 23:22:21 -0700

    running the clock

--- java-training/Tutorials/Clock.java
+++ java-training/Tutorials/Clock.java
@@ -37,5 +37,24 @@ public class Clock implements Runnable {
 	public String getTime() {
 		return hours + ":" + minutes + ":" + seconds;
 	}
+	
+	@Override
+	public void run() {
+		var clock = Clock.getInstance();
+		clock.setTime(12, 59, 50);
+		while (true)
+			try {
+				System.out.println(clock.getTime());
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e);
+			} finally {
+				clock.clockChange();
+			}
+	}
+
+	public static void main(String[] args) {
+		new Clock().run();
+	}
 
 }
