b934b84a764b2fb380ec36536b69c7a922717add
Marouf <maroufibrahimi7264@gmail.com>
2023-09-07 23:16:38 -0700

    how to change second, minute and hour every time

--- java-training/Tutorials/Clock.java
+++ java-training/Tutorials/Clock.java
@@ -12,11 +12,26 @@ public class Clock implements Runnable {
 		}
 		return instance;
 	}
-	
+
 	public void setTime(int hours, int minutes, int seconds) {
 		this.hours = hours;
 		this.minutes = minutes;
 		this.seconds = seconds;
+	}
+
+	public void clockChange() {
+		seconds += 1;
+		if (seconds == 60) {
+			minutes++;
+			seconds = 0;
+		}
+		if (minutes == 60) {
+			hours++;
+			minutes = 0;
+		}
+		if (hours >= 13) {
+			hours = 1;
+		}
 	}
 
 }
