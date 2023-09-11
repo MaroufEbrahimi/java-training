2354e4a072271ea985d8f4743ef6770147ea4add
Marouf <maroufibrahimi7264@gmail.com>
2023-09-10 19:21:56 -0700

    when we use the Descending Array

--- java-training/algorithm/BinarySearch.java
+++ java-training/algorithm/BinarySearch.java
@@ -4,7 +4,10 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 		// when using Binary Search the array must be sorted!
+		// Ascending
 		int arr[] = { 4, 8, 12, 15, 24, 55, 67, 89, 90, 120, 230 };
+		// Descending
+		// int arr[] = { 100, 90, 45, 34, 23, 12, 11, 5, 3, 2, 1 };
 		int res = binary(arr, 0, arr.length - 1, 89);
 		if (res >= 0)
 			System.out.println("Found in index: " + res);
@@ -15,11 +18,14 @@ public class BinarySearch {
 	public static int binary(int array[], int fir, int len, int key) {
 		if (len >= fir) {
 			int mid = (len + fir) / 2;
-			if (array[mid] == key)
+			if (array[mid] == key) {
 				return mid;
-			else if (array[mid] < key)
+			}
+			// When we using Descending Array the condition must be this!
+			// else if (arr[mid] > key)
+			else if (array[mid] < key) {
 				return binary(array, mid + 1, len, key);
-			else
+			} else
 				return binary(array, fir, mid - 1, key);
 		} else
 			return -1;
