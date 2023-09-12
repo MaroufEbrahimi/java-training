b3b8471ecf5bc3e786ef2808d3024af536f403f5
Marouf <maroufibrahimi7264@gmail.com>
2023-09-12 16:45:59 -0700

    how to insert a value at rear of the Queue

--- java-training/data-structure/Queue/Queue.java
+++ java-training/data-structure/Queue/Queue.java
@@ -1,13 +1,29 @@
 
 public class Queue {
-	
+
 	private int size;
 	private int[] queueArray;
 	private int front;
 	private int rear;
 
-	
-	public static void main(String[] args) {
-		
+	// Queue Constructor and Initialization some value
+	public Queue(int s) {
+		size = s;
+		queueArray = new int[size];
+		front = -1;
+		rear = -1;
 	}
+	
+	// Create a function for inserting at the end of Queue (rear)
+	public boolean insert(int data) {
+		boolean res = false;
+		if (rear != queueArray.length - 1) {
+			queueArray[++rear] = data;
+			//maxSize++;
+			res = true;
+		}
+		return res;
+	}
+
+	
 }
