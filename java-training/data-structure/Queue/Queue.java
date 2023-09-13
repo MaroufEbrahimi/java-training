19d51de442de191d3cd5aec285332213e631b172
Marouf <maroufibrahimi7264@gmail.com>
2023-09-12 17:00:40 -0700

    add some other operation on Queue

--- java-training/data-structure/Queue/Queue.java
+++ java-training/data-structure/Queue/Queue.java
@@ -13,17 +13,46 @@ public class Queue {
 		front = -1;
 		rear = -1;
 	}
-	
+
 	// Create a function for inserting at the end of Queue (rear)
 	public boolean insert(int data) {
 		boolean res = false;
 		if (rear != queueArray.length - 1) {
 			queueArray[++rear] = data;
-			//maxSize++;
+			// size++;
 			res = true;
 		}
 		return res;
 	}
 
-	
+	// Removing a value at the front of Queue
+	public int remove() {
+		int res = 0;
+		if (size != 0) {
+			front++;
+			res = queueArray[front];
+			size--;
+		}
+		return res;
+	}
+
+	// Finding the front of element
+	public int peek() {
+		int res = 0;
+		if (!isEmpty()) {
+			res = queueArray[front + 1];
+		}
+		return res;
+	}
+
+	// When the Queue is empty
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+
+	// Finding the size of Queue
+	public int getSize() {
+		return size;
+	}
+
 }
