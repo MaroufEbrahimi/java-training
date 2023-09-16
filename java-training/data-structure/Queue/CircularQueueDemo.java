5e8fa1bf9ad2cfa8e520775619b18acefc6a9b55
Marouf <maroufibrahimi7264@gmail.com>
2023-09-15 17:19:16 -0700

    create: class for showiong elements of Queue

---
+++ java-training/data-structure/Queue/CircularQueueDemo.java
@@ -0,0 +1,23 @@
+
+public class CircularQueueDemo {
+	public static void main(String[] args) {
+		CircularQueue cir = new CircularQueue(6);
+		cir.insert(34);
+		cir.insert(64);
+		cir.insert(340);
+		cir.insert(394);
+		System.out.println(cir.remove());
+		System.out.println(cir.remove());
+		System.out.println(cir.remove());
+		cir.insert(4);
+		cir.insert(5);
+		cir.insert(6);
+		cir.insert(7);
+		
+		while(!cir.isEmpty()) {
+			int rem = cir.remove();
+			System.out.print(rem+" ");
+		}
+		System.out.println();
+	}
+}
