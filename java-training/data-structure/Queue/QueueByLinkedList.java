456cdbf7ffe6fb83f1f6cd913ae7d4a4a8b2a48f
Marouf <maroufibrahimi7264@gmail.com>
2023-09-18 12:07:59 -0700

    another method for displaying elements and create object of Queue

--- java-training/data-structure/Queue/QueueByLinkedList.java
+++ java-training/data-structure/Queue/QueueByLinkedList.java
@@ -1,10 +1,13 @@
+import java.util.NoSuchElementException;
 
 public class QueueByLinkedList {
-
+	
+	// declaration of front, rear, len
 	private QueueNode front;
 	private QueueNode rear;
 	private int len;
 
+	// Creating a Node class
 	private class QueueNode {
 		private int data;
 		private QueueNode next;
@@ -15,20 +18,24 @@ public class QueueByLinkedList {
 		}
 	}
 
+	// assign value for front & rear
 	public QueueByLinkedList() {
 		this.front = null;
 		this.rear = null;
 		len = 0;
 	}
-	
+
+	// finding the length of Queue
 	public int length() {
 		return len;
 	}
 
+	// when the Queue is Empty
 	public boolean isEmpty() {
 		return len == 0;
 	}
-	
+
+	// how to insert element at the end of Queue
 	public void enqueue(int data) {
 		var temp = new QueueNode(data);
 		if (isEmpty()) {
@@ -40,7 +47,63 @@ public class QueueByLinkedList {
 		len++;
 	}
 
-	public static void main(String[] args) {
+	// finding Front
+	public int first() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Queue is Empty");
+		}
 
+		return front.data;
+	}
+
+	// finding Rear
+	public int last() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Queue is Empty");
+		}
+
+		return rear.data;
+	}
+
+	// how to delete element from the front of Queue
+	public int dequeue() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Queue is Empty");
+		}
+		int res = front.data;
+		front = front.next;
+		if (front == null) {
+			rear = null;
+		}
+		len--;
+		return res;
+	}
+
+	// displaying all elements of Queue
+	public void display() {
+		if (isEmpty()) {
+			return;
+		}
+
+		QueueNode current = front;
+		while (current != null) {
+			System.out.print(current.data + " --> ");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+
+	public static void main(String[] args) {
+		var q = new QueueByLinkedList();
+		q.enqueue(10); // insert
+		q.enqueue(34); // insert
+		q.enqueue(20); // insert
+		q.enqueue(64); // insert
+		q.display(); // display
+		q.dequeue(); // removing one element (front)
+		q.display(); // display
+
+		System.out.println("Front: " + q.first());
+		System.out.println("Rear: " + q.last());
 	}
 }
