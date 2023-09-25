c9b953420389cc3d9da03734d2b99cd40297b88f
Marouf <maroufibrahimi7264@gmail.com>
2023-09-25 15:09:44 -0700

    Thread with ArrayList and Vector

--- java-training/Tutorials/VectorTesting.java
+++ java-training/Tutorials/VectorTesting.java
@@ -1,7 +1,9 @@
 package Tutorials;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
 public class VectorTesting {
 
@@ -11,16 +13,61 @@ public class VectorTesting {
 		int size = 1000000;
 
 		// different between Vector and ArrayList
+		// this is an ArrayList
 		List<Integer> list = new ArrayList<>();
 		long startTime = System.currentTimeMillis();
-
 		for (int i = 0; i < size; i++) {
 			list.add(i);
 		}
-
 		long endTime = System.currentTimeMillis();
-
 		System.out.println("Added " + size + " elements to ArrayList: " + (endTime - startTime) + "ms");
+
+		// create a Vector
+		List<Integer> vector = new Vector<>();
+		startTime = System.currentTimeMillis();
+		for (int i = 0; i < size; i++) {
+			vector.add(i);
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println("Added " + size + " elements to Vector: " + (endTime - startTime) + "ms");
+
+		// create a Thread with Array List
+		List<Integer> multiThreadList = Collections.synchronizedList(new ArrayList<>());
+		startTime = System.currentTimeMillis();
+		Thread tr1 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multiThreadList.add(i);
+			}
+		});
+
+		Thread tr2 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multiThreadList.add(i);
+			}
+		});
+		tr1.start();
+		tr2.start();
+		endTime = System.currentTimeMillis();
+		System.out.println("Added " + size + " elements to Thread with ArrayList: " + (endTime - startTime) + "ms");
+
+		// create a Thread with Vector
+		List<Integer> multiThreadVector = new Vector<>();
+		startTime = System.currentTimeMillis();
+		tr1 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multiThreadVector.add(i);
+			}
+		});
+
+		tr2 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multiThreadVector.add(i);
+			}
+		});
+		tr1.start();
+		tr2.start();
+		endTime = System.currentTimeMillis();
+		System.out.println("Added " + size + " elements to Thread with Vector: " + (endTime - startTime) + "ms");
 	}
 
 }
