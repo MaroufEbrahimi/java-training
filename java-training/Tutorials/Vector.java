5dc3bb5319c69113f0a7a7366d78c22d8cd7663f
Marouf <maroufibrahimi7264@gmail.com>
2023-09-25 14:03:01 -0700

    different between Vector and ArrayList

---
+++ java-training/Tutorials/Vector.java
@@ -0,0 +1,26 @@
+package Tutorials;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		// vector is a part of java (is a collection of objects)
+
+		int size = 1000000;
+
+		// different between Vector and ArrayList
+		List<Integer> list = new ArrayList<>();
+		long startTime = System.currentTimeMillis();
+
+		for (int i = 0; i < size; i++) {
+			list.add(i);
+		}
+
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("Added " + size + " elements to ArrayList: " + (endTime - startTime) + "ms");
+	}
+
+}
