b4055ed8a23f87a07e47f84cc9a07fd030f14244
Marouf <maroufibrahimi7264@gmail.com>
2023-09-24 22:13:25 -0700

    sorting an array with Insertion Sort algorithm

---
+++ java-training/algorithm/Sort/InsertionSort/InsertionSort.java
@@ -0,0 +1,29 @@
+package Semester_3;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int list[] = { 1, 6, 3, 0, 12, 4, 3, 43, 44, 13, 43, 434 };
+		System.out.println("Before Sorting:");
+		for (int l : list) {
+			System.out.format("%d, ", l);
+		}
+
+		int i, key, j;
+		for (i = 1; i < list.length; i++) {
+			key = list[i];
+			j = i - 1;
+			while (j >= 0 && list[j] > key) {
+				list[j + 1] = list[j];
+				j--;
+			}
+			list[j + 1] = key;
+		}
+
+		System.out.println("\nAfter Sorting:");
+		for (int l : list) {
+			System.out.format("%d, ", l);
+		}
+	}
+
+}
