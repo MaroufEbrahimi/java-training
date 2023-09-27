ca14b4d03aac6b1ba79531c760c55d82e1e7c133
Marouf <maroufibrahimi7264@gmail.com>
2023-09-26 22:51:52 -0700

    create new Apple and move the snake

--- java-training/Tutorials/SnakeGame/GamePanel.java
+++ java-training/Tutorials/SnakeGame/GamePanel.java
@@ -79,5 +79,33 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 
 	}
+	
+	public void newApple() {
+		appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+		appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+
+	}
+
+	public void move() {
+		for (int i = bodyPart; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
+
+		switch (direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
+	}
 
 }
