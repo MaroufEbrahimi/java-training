1c7a57b43e279cc224acd3e06ab28a96fc89d439
Marouf <maroufibrahimi7264@gmail.com>
2023-09-26 22:12:19 -0700

    added a frame for the game

---
+++ java-training/Tutorials/SnakeGame/GameFrame.java
@@ -0,0 +1,16 @@
+package Snake;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	public GameFrame() {
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
