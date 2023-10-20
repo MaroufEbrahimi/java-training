caf8fcd098ad2e5ca6a34fee99bca791984877e1
Marouf <maroufibrahimi7264@gmail.com>
2023-10-20 14:20:16 -0700

    stream: initial commit

---
+++ java-training/Tutorials/Streams/Gender.java
@@ -0,0 +1,5 @@
+package Streams;
+
+public enum Gender {
+
+}
--- java-training/Tutorials/Streams/Main.java
+++ java-training/Tutorials/Streams/Main.java
@@ -1,9 +1,8 @@
-package LambdaExperissions;
+package Streams;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("salam");
+		
 	}
-
 }
