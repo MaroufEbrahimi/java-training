30741f820428059bced31a7618cd3bd4e2d19064
Marouf <maroufibrahimi7264@gmail.com>
2023-11-17 20:18:48 +0430

    conquer the sub array and combined

--- java-training/algorithm/Sort/Merge Sort/MergeSort.java
+++ java-training/algorithm/Sort/Merge Sort/MergeSort.java
@@ -19,4 +19,34 @@ public class MergeSort {
 		conquer(arr, start, mid, end);
 	}
 
+	public static void conquer(int arr[], int start, int mid, int end) {
+		int merge[] = new int[end - start + 1];
+
+		int i1 = start;
+		int i2 = mid + 1;
+		int a = 0;
+
+		while (i1 <= mid && i2 <= end) {
+			if (arr[i1] <= arr[i2]) {
+				merge[a++] = arr[i1++];
+			} else {
+				merge[a++] = arr[i2++];
+			}
+		}
+
+		while (i1 <= mid) {
+			merge[a++] = arr[i1++];
+		}
+
+		while (i2 <= end) {
+			merge[a++] = arr[i2++];
+		}
+		
+		// sorting elements
+		for (int i = 0, j = start; i < merge.length; i++, j++) {
+			arr[j] = merge[i];
+		}
+		
+	}
+
 }
