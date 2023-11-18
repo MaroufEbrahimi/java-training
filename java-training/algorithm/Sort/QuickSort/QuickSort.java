791d92af0037c50f9b19e3de7f4d075acb8b5dc4
Marouf <maroufibrahimi7264@gmail.com>
2023-11-18 19:01:42 +0430

    swap the element

--- java-training/algorithm/Sort/QuickSort/QuickSort.java
+++ java-training/algorithm/Sort/QuickSort/QuickSort.java
@@ -40,4 +40,10 @@ public class QuickSort {
 
 	}
 
+	public static void swap(int[] arr, int index1, int index2) {
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+
 }
