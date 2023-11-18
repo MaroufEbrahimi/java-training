c86bcc51c00ab40d19c892b94134d50bdb7065e3
Marouf <maroufibrahimi7264@gmail.com>
2023-11-18 19:00:00 +0430

    method for find pivot and sorting

--- java-training/algorithm/Sort/QuickSort/QuickSort.java
+++ java-training/algorithm/Sort/QuickSort/QuickSort.java
@@ -11,4 +11,33 @@ public class QuickSort {
 		}
 	}
 
+	public static void quickSort(int arr[], int lowIndex, int highIndex) {
+		if (lowIndex >= highIndex) {
+			return;
+		}
+
+		int pivot = arr[highIndex];
+
+		int leftPointer = lowIndex;
+		int rightPointer = highIndex;
+
+		while (leftPointer < rightPointer) {
+			while (arr[leftPointer] <= pivot && leftPointer < rightPointer) {
+				leftPointer++;
+			}
+
+			while (arr[rightPointer] >= pivot && leftPointer < rightPointer) {
+				rightPointer--;
+			}
+
+			swap(arr, leftPointer, rightPointer);
+		}
+
+		swap(arr, leftPointer, highIndex);
+
+		quickSort(arr, lowIndex, leftPointer - 1);
+		quickSort(arr, leftPointer + 1, highIndex);
+
+	}
+
 }
